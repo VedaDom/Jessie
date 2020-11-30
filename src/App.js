3a19fe596c16baddd794273d59757e78699018e1
Nsengimana Veda Dominique <nsengimanavedadom@gmail.com>
2020-11-30 10:10:08 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import axios from "axios";
 
 import SimplexNoise from "simplex-noise";
 
+import { useSpring, a } from "react-spring/three";
+
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+
 var noise = new SimplexNoise();
 
 function fractionate(val, minVal, maxVal) {
@@ -30,21 +35,81 @@ function max(arr){
   return arr.reduce(function(a, b){ return Math.max(a, b); })
 }
 
-function Sphere() {
+const wit = async (message) => {
+  var config = {
+    method: 'get',
+    url: `https://api.wit.ai/message?v=20201021&q=${message}`,
+    headers: { 
+      'Authorization': 'Bearer UQJVPTTDPVX4MQPHBPKZMJBVC3SVG3JV'
+    }
+  };
+  
+  const res = await axios(config);
+  return res.data;
+}
+
+const Sphere = () => {
 
   const ref = useRef(null);
   const mesh = useRef();
 
   const [ready, setReady] = useState(false);
   const [dataArray, setDataArray] = useState();
+  const [isInCenter, setIsInCenter] = useState(true);
   // const [text, setText] = useState("Hello! is there anyone? If you're, You can call me Jessie. Feel free to call me if you need any help.");
   const [text, setText] = useState("Hello! I'm Jessie. Your personal assistant. To get started, tell me some things about yourself and what can I help you with.");
 
   const audioContext = useRef(new (window.AudioContext || window.webkitAudioContext)());
-  const analyser = useRef(audioContext.current.createAnalyser());  
+  const analyser = useRef(audioContext.current.createAnalyser());
+
+  const props = useSpring({
+    position: isInCenter ? [0, 0, 0] : [-30, 0, 0]
+  })
+
+  const { transcript, resetTranscript, interimTranscript, listening } = useSpeechRecognition();
+  const [isListening, setIsListening] = useState(false);
+  const [message, setMessage] = useState('');
+  // if (!SpeechRecognition.browserSupportsSpeechRecognition) {
+  //   console.log('Not supported!');
+  // }
+
+  SpeechRecognition.startListening({ continuous: true });
+
+  const getSpeech = async (text) => {
+    const response = await axios.post(
+      "https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/490efb2e-9938-42c0-84ee-73f960ad79aa/v1/synthesize?voice=en-GB_CharlotteV3Voice",
+      {
+        text: text
+      }, 
+      {
+        responseType: 'arraybuffer',
+        headers: {
+          'Accept': 'audio/wav',
+        },
+        auth: {
+          username: 'apikey',
+          password: "1tX36zmqF2Km0rLydaGBRD_RPzOrzOQnHvm_oD6QpeJS"
+        }
+      }
+    );
+    const source = audioContext.current.createBufferSource();
+    audioContext.current.decodeAudioData(response.data).then((audioBuffer) => {
+      source.buffer = audioBuffer;
+    });
+    source.connect(analyser.current);
+    analyser.current.connect(audioContext.current.destination);
+    analyser.current.fftSize = 2048;
+    var bufferLength = analyser.current.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
+    setDataArray(dataArray);
+    source.start(0);
+    setReady(true);
+  }
 
   useFrame(() => {
-    mesh.current.rotation.y += 0.001;
+    if (!isListening) {
+      mesh.current.rotation.y += 0.001;
+    }
     if (!ready) {
       return;
     }
@@ -76,44 +141,39 @@ function Sphere() {
 
 
   useEffect(() => {
-    const getSpeech = async () => {
-      const response = await axios.post(
-        "https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/3c51f216-127f-45fb-85c4-b4ffc8793603/v1/synthesize?voice=en-GB_CharlotteV3Voice",
-        {
-          text: text
-        }, 
-        {
-          responseType: 'arraybuffer',
-          headers: {
-            'Accept': 'audio/wav',
-          },
-          auth: {
-            username: 'apikey',
-            password: "hiw4yOfvlRDJacJVfgQwqJdBImNgWfC92mm-akoWSdvv"
-          }
-        }
-      );
-      const source = audioContext.current.createBufferSource();
-      audioContext.current.decodeAudioData(response.data).then((audioBuffer) => {
-        source.buffer = audioBuffer;
-      });
-      source.connect(analyser.current);
-      analyser.current.connect(audioContext.current.destination);
-      analyser.current.fftSize = 2048;
-      var bufferLength = analyser.current.frequencyBinCount;
-      var dataArray = new Uint8Array(bufferLength);
-      setDataArray(dataArray);
-      source.start(0);
-      setReady(true);
+    if (interimTranscript.toLowerCase().includes('jessie')) {
+      setIsListening(true);
     }
-    getSpeech();
-  }, [text]);
+    if (isListening && message.length > 1 && interimTranscript === "") {
+      setIsListening(false);
+      wit(message).then((data) => {
+        if (data.intents[0] === undefined) {
+          return getSpeech(`I do not understand what; "${data.text}" mean. Would you like to help me on that?`);
+        }
+        if (data.traits[`wit_${data.intents[0].name}`] === undefined) {
+          getSpeech(`I do not understand what; "${data.text}" mean. Would you like to help me on that?`);
+        } else{
+          getSpeech(data.traits[`wit_${data.intents[0].name}`][0].value);
+        }
+      });
+      setMessage("");
+    }
+    if (isListening && !interimTranscript.toLowerCase().includes('jessie') && interimTranscript.length > 1) {
+      setMessage(interimTranscript);
+    }
+  });
 
+  const onClick = (e) => {
+    audioContext.current.resume().then(() => {
+      console.log('resumed');
+    })
+  }
 
   return(
-  <mesh
+  <a.mesh
     ref={mesh}
-    position={[0, 0, 0]}>
+    position={props.position}
+    onClick={onClick}>
       <icosahedronGeometry
         ref={ref}
         attach="geometry"
@@ -124,15 +184,13 @@ function Sphere() {
         color={0xff00ee}
         wireframe={true}
       />
-  </mesh>
+  </a.mesh>
   );
 }
 
-function App() {
-  
+const App = () => {
   return (
     <>
-    
     <Canvas
       camera={{position: [0, 0, 100], fov: 45}}>
       <ambientLight
